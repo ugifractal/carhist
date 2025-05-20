e10933d965f0864947564615c13818a6d803895d
ugifractal <ugidmtest@gmail.com>
2025-05-20 10:02:38 +0700

    [#191] add form for car fueling

--- db/schema.rb
+++ db/schema.rb
@@ -10,37 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_18_112809) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_20_024416) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
-
-  create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
-  end
-
-  create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
-    t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
 
   create_table "car_brands", force: :cascade do |t|
     t.string "name"
@@ -48,6 +20,19 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_18_112809) do
     t.datetime "updated"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "car_fuelings", force: :cascade do |t|
+    t.integer "car_id"
+    t.datetime "filled_at"
+    t.integer "price"
+    t.integer "km"
+    t.integer "ron_number"
+    t.string "location"
+    t.integer "fuel_range"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "volume"
   end
 
   create_table "car_maintenances", force: :cascade do |t|
@@ -97,7 +82,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_18_112809) do
     t.datetime "updated"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.jsonb "image_data"
   end
 
   create_table "history_images", force: :cascade do |t|
@@ -127,7 +111,4 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_18_112809) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
 end
