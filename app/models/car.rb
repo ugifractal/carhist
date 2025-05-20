66a1781ef9a2793081625b399da31ee77a328066
ugifractal <ugidmtest@gmail.com>
2025-05-20 09:07:12 +0700

    Fix indentation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,12 +1,12 @@
 class Car < ApplicationRecord
   include ImageUploader::Attachment(:image)
-    belongs_to :user
-    belongs_to :car_model
-    has_many :car_maintenances, dependent: :destroy
+  belongs_to :user
+  belongs_to :car_model
+  has_many :car_maintenances, dependent: :destroy
 
-    validates :name, presence: true
+  validates :name, presence: true
 
-    def brand_name
-      "#{car_model.car_brand.name} #{car_model.model} #{car_model.year}"
-    end
+  def brand_name
+    "#{car_model.car_brand.name} #{car_model.model} #{car_model.year}"
   end
+end
