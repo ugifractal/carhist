a68ff7b3b6464f049941c397fd193626bc31eac6
ugifractal <ugidmtest@gmail.com>
2025-05-21 13:19:12 +0700

    rubocop fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :avatar ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :avatar ])
   end
 
   private
