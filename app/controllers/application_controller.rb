b5406297acf6497afada55068642c6c610d4a91e
ugifractal <ugidmtest@gmail.com>
2025-05-21 13:16:16 +0700

    Add support for mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  allow_browser versions: { safari: 16.4, firefox: 121, ie: false }
 
   before_action :set_cars
   before_action :set_locale
