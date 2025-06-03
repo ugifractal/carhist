f7625a71ef043fd330e41187a2c1f8e413a28df4
unknown <tsaqibarib@gmail.com>
2025-06-03 07:15:20 +0700

    initial

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,6 +1,8 @@
 module Api
   module V1
     class CarsController < ApplicationController
+      before_action :authenticate_api_key!
+
       def index
         cars = [
           { id: 1, name: 'captiva', brand: 'chevrolet' },
@@ -9,6 +11,16 @@ module Api
 
         render json: { cars: cars }
       end
+
+      private
+
+      def authenticate_api_key!
+        header_key = request.headers['api-key']
+        unless ApiKey.exists?(key: header_key)
+          render json: { error: 'Unauthorized. Invalid or missing API key.' }, status: :unauthorized
+        end
+      end
+
     end
   end
 end
