09b3b254a74f324c749dd9c4a4e4f4c748156b75
unknown <tsaqibarib@gmail.com>
2025-06-06 20:16:13 +0700

    [#223] fix apikey

--- app/controllers/api/v1/cars_controller.rb
+++ app/controllers/api/v1/cars_controller.rb
@@ -1,26 +1,21 @@
 module Api
   module V1
     class CarsController < ApplicationController
-      before_action :authenticate_api_key!
+      ##before_action :authenticate_api_key!
 
       def index
-        cars = [
-          { id: 1, name: 'captiva', brand: 'chevrolet' },
-          { id: 2, name: 'trax', brand: 'chevrolet' }
-        ]
-
-        render json: { cars: cars }
+        cars = Car.all
+        render json: { cars: cars.as_json(only: [:id, :name, :brand]) }
       end
 
       private
 
       def authenticate_api_key!
-        header_key = request.headers['api-key']
+        header_key = request.headers["api-key"]
         unless ApiKey.exists?(key: header_key)
-          render json: { error: 'Unauthorized. Invalid or missing API key.' }, status: :unauthorized
+          render json: { error: "Unauthorized. Invalid or missing API key." }, status: :unauthorized
         end
       end
-
     end
   end
 end
