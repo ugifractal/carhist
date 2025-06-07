2891c0d8ff41c2a1636511a7792623056e45d1e5
unknown <tsaqibarib@gmail.com>
2025-06-07 20:44:30 +0700

    fix

--- app/models/car.rb
+++ app/models/car.rb
@@ -10,4 +10,29 @@ class Car < ApplicationRecord
   def brand_name
     "#{car_model.car_brand.name} #{car_model.model} #{car_model.year}"
   end
+
+  def generate_maintenance_pdf
+    maintenances = car_maintenances.order(performed_at: :desc)
+
+    Prawn::Document.new do |pdf|
+      pdf.text "Maintenance Report for #{name} (#{car_model.car_brand.name})", size: 16, style: :bold
+      pdf.move_down 10
+
+      if maintenances.any?
+        table_data = [["Date", "Type", "Title", "Description"]]
+        maintenances.each do |m|
+          table_data << [
+            m.performed_at&.strftime("%d %b %Y"),
+            m.maintenance_type.humanize,
+            m.title,
+            m.description
+          ]
+        end
+
+        pdf.table(table_data, header: true, row_colors: %w[F0F0F0 FFFFFF], cell_style: { borders: [] })
+      else
+        pdf.text "No maintenance records found.", style: :italic
+      end
+    end.render
+  end
 end
