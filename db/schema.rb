baecac10016d938fa8d0e316ae94ee455e7d14fd
unknown <tsaqibarib@gmail.com>
2025-06-13 11:08:18 +0700

    [#258] fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_11_031756) do
+ActiveRecord::Schema[8.0].define(version: 2025_06_13_014128) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -81,6 +81,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_06_11_031756) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "car_shop_id"
   end
 
   create_table "car_models", force: :cascade do |t|
