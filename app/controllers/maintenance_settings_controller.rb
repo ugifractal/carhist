b7d570a80b558a235d9d517f6acebab28aace8c4
unknown <tsaqibarib@gmail.com>
2025-06-21 05:50:24 +0700

    [#297] add several form

--- app/controllers/maintenance_settings_controller.rb
+++ app/controllers/maintenance_settings_controller.rb
@@ -1,5 +1,23 @@
 class MaintenanceSettingsController < ApplicationController
   def show
     @car = Car.find(params[:car_id])
+    @maintenance_setting = @car.maintenance_setting || @car.build_maintenance_setting
+  end
+
+  def update
+    @car = Car.find(params[:car_id])
+    @maintenance_setting = @car.maintenance_setting
+
+    if @maintenance_setting.update(maintenance_setting_params)
+      redirect_to car_maintenance_setting_path(@car), notice: "Settings updated successfully"
+    else
+      render :show
+    end
+  end
+
+  private
+
+  def maintenance_setting_params
+    params.require(:maintenance_setting).permit(:engine_oil_change_interval, :transmission_oil_change_interval)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions", passwords: "users/passwords", registrations: "users/registrations", confirmations: "users/confirmations" }
 
   resources :cars do
-    resource :maintenance_settings, only: [:show]
+    resource :maintenance_settings
     resources :car_maintenances do
       collection do
         get :export_pdf
