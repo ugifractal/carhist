2056994255a6de9f305a056d1ad3e636bef5c088
unknown <tsaqibarib@gmail.com>
2025-07-07 05:54:12 +0700

    [#309] add new field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_03_110743) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_06_225011) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -109,6 +109,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_03_110743) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.string "lat"
+    t.string "lng"
   end
 
   create_table "cars", force: :cascade do |t|
