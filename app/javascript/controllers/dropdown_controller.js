5c7a13b2747d47559cf53d781e714f59430adc55
unknown <tsaqibarib@gmail.com>
2025-07-08 09:26:47 +0700

    [#337] edit dropdown

--- app/javascript/controllers/dropdown_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus"
-
-export default class extends Controller {
-  static targets = ["menu"]
-
-  toggle() {
-    this.menuTarget.classList.toggle("hidden")
-  }
-}
