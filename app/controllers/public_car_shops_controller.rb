7bcf8ea69cd8b8382f09e2fa34813b8ca09a18bb
tsaqib29 <tsaqibarib@.com>
2025-07-15 09:36:32 +0700

    [#341] fix carshop

--- app/controllers/public_car_shops_controller.rb
+++ app/controllers/public_car_shops_controller.rb
@@ -1,5 +1,14 @@
-class PublicCarShopsController < ApplicationController
-  def show
-    @car_shop = CarShop.find_by(id: params[:id], approved_at: !nil)
+module Public
+  class CarShopsController < ApplicationController
+    def show
+      @car_shop = CarShop.find_by(id: params[:id], approved_at: !nil)
+
+      if @car_shop.nil?
+      render plain: "Car Shop not found or not approved", status: :not_found
+      elsif params[:slug] != @car_shop.name.parameterize
+      redirect_to public_car_shop_path(@car_shop.id, @car_shop.name.parameterize), status: :moved_permanently
+    end
+    end
   end
 end
+
