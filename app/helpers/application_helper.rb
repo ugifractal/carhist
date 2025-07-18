146d1cd6013846bd603189401700aa6a895ea3c0
ugifractal <ugidmtest@gmail.com>
2025-07-18 16:00:24 +0700

    add highlight

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def dashboard_page?
+    controller_name == "dashboards"
+  end
+
+  def car_page?
+    controller_name == "cars"
+  end
+
+  def car_shop_page?
+    controller_name == "car_shops"
+  end
 end
