a3d9e7f8f547bb3c27aaf28a34b3dc6f25647224
tsaqib29 <tsaqibarib@.com>
2025-07-22 19:26:34 +0700

    [349] add link car shop

--- app/controllers/public/car_shops_controller.rb
+++ app/controllers/public/car_shops_controller.rb
@@ -14,11 +14,7 @@ module Public
     def show
       @car_shop = CarShop.approved.find(params[:id])
 
-      if @car_shop.nil?
-        redirect_to root_path, alert: "Car shop not found or not approved."
-      else
-        render layout: "welcome"
-      end
+      render layout: "welcome"
     end
   end
 end
