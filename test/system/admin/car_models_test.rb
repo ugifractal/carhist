e81a67fcae8fa8320f979d91e7edfa0427854fce
tsaqib29 <tsaqibarib@.com>
2025-08-14 16:48:35 +0700

    [#375] bug, uncommand

--- test/system/admin/car_models_test.rb
+++ test/system/admin/car_models_test.rb
@@ -1,76 +1,76 @@
-# require "application_system_test_case"
+require "application_system_test_case"
 
-# class CarModelsTest < ApplicationSystemTestCase
-#   include Devise::Test::IntegrationHelpers
+class CarModelsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
 
-#   setup do
-#     @user = users(:admin)
-#     login_as @user
-#     @car_brand = car_brands(:suzuki)
-#     @car_model = car_models(:escudo)
-#   end
+  setup do
+    @user = users(:admin)
+    login_as @user
+    @car_brand = car_brands(:suzuki)
+    @car_model = car_models(:escudo)
+  end
 
-#   test "visiting the index" do
-#     visit admin_car_models_url
-#     assert_text "Your Car Models"
-#     assert_text @car_model.model
-#   end
+  test "visiting the index" do
+    visit admin_car_models_url
+    assert_text "Your Car Models"
+    assert_text @car_model.model
+  end
 
-#   test "create model" do
-#     user = users(:admin)
-#     login_as user
+  test "create model" do
+    user = users(:admin)
+    login_as user
 
-#     car_model = car_models(:escudo)
+    car_model = car_models(:escudo)
 
-#     visit new_admin_car_model_path(car_model)
-#     sleep 1
-#     select "suzuki", from: "Brand"
-#     fill_in "Model", with: "Pajero"
-#     fill_in "Year", with: 2030
-#     fill_in "Overall width", with: 2000
-#     fill_in "Overall height", with: 2000
-#     fill_in "Overall length", with: 2000
-#     fill_in "Wheelbase", with: 2000
-#     fill_in "Tank capacity", with: 2000
-#     click_button "Create"
-#     sleep 1
-#     assert_text "Car model has been created!"
-#   end
+    visit new_admin_car_model_path(car_model)
+    sleep 1
+    select "suzuki", from: "Brand"
+    fill_in "Model", with: "Pajero"
+    fill_in "Year", with: 2030
+    fill_in "Overall width", with: 2000
+    fill_in "Overall height", with: 2000
+    fill_in "Overall length", with: 2000
+    fill_in "Wheelbase", with: 2000
+    fill_in "Tank capacity", with: 2000
+    click_button "Create"
+    sleep 1
+    assert_text "Car model has been created!"
+  end
 
-#   test "edit model" do
-#     user = users(:admin)
-#     login_as user
+  test "edit model" do
+    user = users(:admin)
+    login_as user
 
-#     car_model = car_models(:escudo)
+    car_model = car_models(:escudo)
 
-#     visit edit_admin_car_model_path(car_model)
-#     sleep 1
-#     select "suzuki", from: "Brand"
-#     fill_in "Model", with: "Brio"
-#     fill_in "Year", with: 2025
-#     fill_in "Overall width", with: 1800
-#     fill_in "Overall height", with: 1400
-#     fill_in "Overall length", with: 3800
-#     fill_in "Wheelbase", with: 2500
-#     fill_in "Tank capacity", with: 50
-#     click_button "Update"
-#     sleep 1
-#     assert_text "Car model was successfully updated."
-#   end
+    visit edit_admin_car_model_path(car_model)
+    sleep 1
+    select "suzuki", from: "Brand"
+    fill_in "Model", with: "Brio"
+    fill_in "Year", with: 2025
+    fill_in "Overall width", with: 1800
+    fill_in "Overall height", with: 1400
+    fill_in "Overall length", with: 3800
+    fill_in "Wheelbase", with: 2500
+    fill_in "Tank capacity", with: 50
+    click_button "Update"
+    sleep 1
+    assert_text "Car model was successfully updated."
+  end
 
-#   test "destroy" do
-#     user = users(:admin)
-#     login_as user
+  test "destroy" do
+    user = users(:admin)
+    login_as user
 
-#     car_model = car_models(:escudo)
+    car_model = car_models(:escudo)
 
-#     visit admin_car_models_url
-#     sleep 1
+    visit admin_car_models_url
+    sleep 1
 
-#     accept_confirm do
-#       page.find(:xpath, "//tbody/tr[position()=1]/td[position()=5]/*/button[@title='Delete']").click
-#     end
-#     sleep 1
-#     assert_text "Car model was successfully destroyed."
-#   end
-# end
+    accept_confirm do
+      page.find(:xpath, "//tbody/tr[position()=1]/td[position()=5]/*/button[@title='Delete']").click
+    end
+    sleep 1
+    assert_text "Car model was successfully destroyed."
+  end
+end
