21b6fcf883c2e08de795365f658ca62c7d114b49
tsaqib29 <tsaqibarib@.com>
2025-08-16 07:19:06 +0700

    [#385] add car maintenance test

---
+++ test/system/car_maintenances_test.rb
@@ -0,0 +1,60 @@
+require "application_system_test_case"
+
+class CarMaintenancesTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  test "visiting the index" do
+    user = users(:sakib)
+    login_as user
+
+    car_maintenance = car_maintenances(:history)
+    car = cars(:skyline)
+    visit "/cars/#{car.id}/car_maintenances"
+    sleep 1
+    assert_text "Service Record dari"
+  end
+
+  test "create car maintenance" do
+    user = users(:sakib)
+    login_as user
+
+    car = cars(:skyline)
+
+    visit "/cars/#{car.id}/car_maintenances/new"
+    sleep 1
+
+    select "AC Repair", from: "car_maintenance_maintenance_type"
+    fill_in "car_maintenance_title", with: "ganti oli"
+    select "30", from: "car_maintenance_performed_at_3i"
+    select "July", from: "car_maintenance_performed_at_2i"
+    select "2022", from: "car_maintenance_performed_at_1i"
+    fill_in "car_maintenance_description", with: "ini deskripsi"
+    select "garuda", from: "car_maintenance_car_shop_id"
+    click_button "Buat Service Record"
+    sleep 1
+    visit "/cars/#{car.id}/car_maintenances"
+  end
+
+  test "edit car maintenance" do
+    user = users(:sakib)
+    login_as user
+
+    car = cars(:skyline)
+    car_maintenance = car_maintenances(:history)
+
+    visit "/cars/#{car.id}/car_maintenances/#{car_maintenance.id}/edit"
+    sleep 1
+
+    select "AC Repair", from: "car_maintenance_maintenance_type"
+    fill_in "car_maintenance_title", with: "ganti ban"
+    select "12", from: "car_maintenance_performed_at_3i"
+    select "June", from: "car_maintenance_performed_at_2i"
+    select "2023", from: "car_maintenance_performed_at_1i"
+    fill_in "car_maintenance_description", with: "ini edit deskripsi"
+    select "garuda", from: "car_maintenance_car_shop_id"
+    click_button "Update Service Record"
+    sleep 1
+    visit "/cars/#{car.id}/car_maintenances"
+    assert_text "Car maintenance was successfully updated."
+  end
+end
