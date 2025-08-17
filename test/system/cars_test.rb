a67816dd259142e9ccb7a056d38048f400a34a60
ugifractal <ugidmtest@gmail.com>
2025-08-18 06:44:28 +0700

    Activate all test

--- test/system/cars_test.rb
+++ test/system/cars_test.rb
@@ -1,69 +1,69 @@
-# require "application_system_test_case"
+require "application_system_test_case"
 
-# class CarsTest < ApplicationSystemTestCase
-#   include Devise::Test::IntegrationHelpers
+class CarsTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
 
-#   test "visiting the index" do
-#     user = users(:sakib)
-#     login_as user
+  test "visiting the index" do
+    user = users(:sakib)
+    login_as user
 
-#     car = cars(:skyline)
+    car = cars(:skyline)
 
-#     visit cars_url
-#     sleep 1
-#     assert_text "Daftar Mobil"
-#     assert_text "skyline"
-#   end
+    visit cars_url
+    sleep 1
+    assert_text "Daftar Mobil"
+    assert_text "skyline"
+  end
 
-#   test "create car" do
-#     user = users(:sakib)
-#     login_as user
+  test "create car" do
+    user = users(:sakib)
+    login_as user
 
-#     car = cars(:skyline)
-#     model = car_models(:escudo)
+    car = cars(:skyline)
+    model = car_models(:escudo)
 
-#     visit new_car_path(car)
-#     sleep 1
+    visit new_car_path(car)
+    sleep 1
 
-#     fill_in "car_name", with: "Pajero"
-#     select "2024", from: "car_year"
-#     select "suzuki Escudo", from: "car_car_model_id"
-#     click_button "Tambah"
-#     sleep 1
-#     assert_text "Car was successfully created."
-#   end
+    fill_in "car_name", with: "Pajero"
+    select "2024", from: "car_year"
+    select "suzuki Escudo", from: "car_car_model_id"
+    click_button "Tambah"
+    sleep 1
+    assert_text "Car was successfully created."
+  end
 
-#   test "edit car" do
-#     user = users(:sakib)
-#     login_as user
+  test "edit car" do
+    user = users(:sakib)
+    login_as user
 
-#     car = cars(:skyline)
-#     model = car_models(:escudo)
+    car = cars(:skyline)
+    model = car_models(:escudo)
 
-#     visit edit_car_path(car)
-#     sleep 1
+    visit edit_car_path(car)
+    sleep 1
 
-#     fill_in "car_name", with: "avanza"
-#     select "2021", from: "car_year"
-#     select "nissan skyline", from: "car_car_model_id"
-#     click_button "Update"
-#     sleep 1
-#     assert_text "Car was successfully updated."
-#   end
+    fill_in "car_name", with: "avanza"
+    select "2021", from: "car_year"
+    select "nissan skyline", from: "car_car_model_id"
+    click_button "Update"
+    sleep 1
+    assert_text "Car was successfully updated."
+  end
 
-#   test "destroy" do
-#     user = users(:sakib)
-#     login_as user
+  test "destroy" do
+    user = users(:sakib)
+    login_as user
 
-#     car = cars(:skyline)
+    car = cars(:skyline)
 
-#     visit cars_path
-#     sleep 1
+    visit cars_path
+    sleep 1
 
-#     accept_confirm do
-#       page.find(:xpath, "//tbody/tr[1]//button[@data-turbo-confirm='Are you sure?']").click
-#     end
+    accept_confirm do
+      page.find(:xpath, "//tbody/tr[1]//button[@data-turbo-confirm='Are you sure?']").click
+    end
 
-#     assert_text "Car was successfully destroyed."
-#   end
-# end
+    assert_text "Car was successfully destroyed."
+  end
+end
