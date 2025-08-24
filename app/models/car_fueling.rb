ccc57fc1a32758a0eec7c3384318ee1b5450a886
ugifractal <ugidmtest@gmail.com>
2025-08-25 06:18:22 +0700

    move formatting to helper

--- app/models/car_fueling.rb
+++ app/models/car_fueling.rb
@@ -4,6 +4,8 @@ class CarFueling < ApplicationRecord
   validates :distance, numericality: { only_integer: false, greater_than: 0 }, presence: true
 
   def fuel_avg
+    return 0 if volume.zero?
+
     distance / volume
   end
 end
