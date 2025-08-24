808c3724a69715de78a9d03e80c48613615b59e6
ugifractal <ugidmtest@gmail.com>
2025-08-25 06:07:04 +0700

    improve locale

--- app/models/car_fueling.rb
+++ app/models/car_fueling.rb
@@ -1,11 +1,9 @@
 class CarFueling < ApplicationRecord
   belongs_to :car
   validates :volume, numericality: { only_integer: false, greater_than: 0 }, presence: true
+  validates :distance, numericality: { only_integer: false, greater_than: 0 }, presence: true
 
   def fuel_avg
-    return nil if volume.nil? || distance.nil?
-    return nil if volume.zero?
-
-    "1 : #{(distance / volume).round(1)}"
+    distance / volume
   end
 end
