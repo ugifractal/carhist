68f2255700d3d7a308c10ddea76baf56d604ba25
ugifractal <ugidmtest@gmail.com>
2025-08-25 07:57:02 +0700

    add test for account

---
+++ test/system/account_test.rb
@@ -0,0 +1,22 @@
+require "application_system_test_case"
+
+class AccountTest < ApplicationSystemTestCase
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    # Set language
+    I18n.locale = :id
+  end
+
+  test "open edit user from right menu" do
+    user = users(:sakib)
+    login_as user
+    visit dashboards_path
+    sleep 1
+
+    page.find(:xpath, "//div[@data-btn='right-top-menu']").click
+    page.find(:xpath, "//a[contains(text(), '#{user.email}')]").click
+    sleep 1
+    assert_text "Edit User"
+  end
+end
