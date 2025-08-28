15f281c2aebc18d0f4c95537fdfe2dd31c0098fc
ugifractal <ugidmtest@gmail.com>
2025-08-28 08:50:26 +0700

    gemini for car spec

--- app/controllers/admin/ai_helps_controller.rb
+++ app/controllers/admin/ai_helps_controller.rb
@@ -1,9 +1,9 @@
 module Admin
   class AiHelpsController < AdminBaseController
     def car_spec
-      data = request.body.read
+      data = JSON.parse(request.body.read)
       AiUtil::GeminiCaller.setup(ENV.fetch("GEMINI_API_KEY"))
-      response = AiUtil::CarModelAi.grab_car_spec(model: data[:model], year: data[:year])
+      response = AiUtil::CarModelAi.grab_car_spec(model: data["model"], year: data["year"])
 
       render json: { output: response[:text] }
     end
--- app/javascript/controllers/ai_helper/car_model_controller.js
+++ app/javascript/controllers/ai_helper/car_model_controller.js
@@ -1,6 +1,7 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = ["button"]
   static values = {
     url: String,
     model: String,
@@ -12,6 +13,10 @@ export default class extends Controller {
 
   async fill(e){
     e.preventDefault()
+    this.buttonTarget.disabled = true
+    let originalText = this.buttonTarget.textContent
+    this.buttonTarget.textContent = "Calling AI..."
+
     const meta = document.querySelector('meta[name="csrf-token"]')
     let token = (meta == null)? '' : meta.getAttribute('content')
 
@@ -25,7 +30,15 @@ export default class extends Controller {
       body: JSON.stringify({model: this.modelValue, year: this.yearValue})
     })
     const json = await response.json()
+    const data = JSON.parse(json.output)
+    Object.keys(data).forEach(key => {
+      if (document.getElementById(`car_model_${key}`)){
+        const input = document.getElementById(`car_model_${key}`)
+        input.value = data[key]
+      }
+    })
 
-    const data = JSON.parse(json.text)
+    this.buttonTarget.disabled = false
+    this.buttonTarget.textContent = originalText
   }
 }
