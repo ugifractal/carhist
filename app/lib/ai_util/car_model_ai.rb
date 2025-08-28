5520a63308f7b2a353e96402b69b68f265040198
ugifractal <ugidmtest@gmail.com>
2025-08-28 08:55:10 +0700

    lint

--- app/lib/ai_util/car_model_ai.rb
+++ app/lib/ai_util/car_model_ai.rb
@@ -2,13 +2,13 @@ module AiUtil
   class CarModelAi < GeminiCaller
     class << self
       def grab_car_spec(model:, year:)
-        prompt = %Q{
+        prompt = %Q(
           get some information about car with model #{model} #{year} for indonesia market build a json response without markdown formating with following fields:
           - width dimension as 'overall_width' in mm unit
           - length dimension as 'overall_length' in mm unit
           - wheelbase dimension as 'wheelbase' in mm unit
           - tank capacity dimension as 'wheelbase' in litre unit
-        }
+        )
         puts api_key
         request(prompt)
       end
