fd22e4d352e9c387801ed3776912950242f71fdf
tsaqib29 <tsaqibarib@.com>
2025-08-30 16:18:58 +0700

    [#419] remove migration

--- db/migrate/20250828072858_add_filled_at_to_car_maintenances.rb
+++
@@ -1,5 +0,0 @@
-class AddFilledAtToCarMaintenances < ActiveRecord::Migration[8.0]
-  def change
-    add_column :car_maintenances, :filled_at, :datetime
-  end
-end
--- db/migrate/20250828235651_remove_filled_at_from_car_maintenances.rb
+++
@@ -1,5 +0,0 @@
-class RemoveFilledAtFromCarMaintenances < ActiveRecord::Migration[8.0]
-  def change
-    remove_column :car_maintenances, :filled_at, :datetime
-  end
-end
