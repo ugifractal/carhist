7dd609fe06bd3da6213db6f741dcc973b2b82d62
tsaqib29 <tsaqibarib@.com>
2025-09-02 20:10:14 +0700

    [#427] fix error

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,6 @@ class DashboardsController < ApplicationController
   def show
     @pending_order = current_user.company.pending_order
     @cars = current_user.company.cars
-
     @recent_service = CarMaintenance
       .joins(:car)
       .where(cars: { company_id: current_user.company_id })
