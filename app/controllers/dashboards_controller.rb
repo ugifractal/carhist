a677d6a7489d1ea173c4fb8ac416c65986337a80
tsaqib29 <tsaqibarib@.com>
2025-09-03 07:17:52 +0700

    [#427 change controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -4,7 +4,7 @@ class DashboardsController < ApplicationController
   def show
     @pending_order = current_user.company.pending_order
     @cars = current_user.company.cars
-    @recent_service = CarMaintenance
+    @recent_service = current_user.company.car_maintenances
       .joins(:car)
       .where(cars: { company_id: current_user.company_id })
       .order(performed_at: :desc)
--- app/models/company.rb
+++ app/models/company.rb
@@ -2,6 +2,7 @@ class Company < ApplicationRecord
   has_many :car_shops, dependent: :destroy
   has_many :orders, dependent: :destroy
   has_many :cars, dependent: :destroy
+  has_many :car_maintenances, through: :cars
 
   def pending_order
     orders.where(status: "pending").order(created_at: :desc).last
