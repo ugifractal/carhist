bad7c4eb032a159cc2b4d480b4f26ca029c04e9e
tsaqib29 <tsaqibarib@.com>
2025-09-13 10:10:10 +0700

    [#427] change controller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,8 +5,6 @@ class DashboardsController < ApplicationController
     @pending_order = current_user.company.pending_order
     @cars = current_user.company.cars
     @recent_service = current_user.company.car_maintenances
-      .joins(:car)
-      .where(cars: { company_id: current_user.company_id })
       .order(performed_at: :desc)
       .first
   end
