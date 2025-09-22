9cfc7dc68159e76ab4f498784627a274f5c297e9
tsaqib29 <tsaqibarib@.com>
2025-09-22 18:43:20 +0700

    [#427] fix gemini analys

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,11 +1,19 @@
 class DashboardsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_company
 
   def show
-    @pending_order = current_user.company.pending_order
-    @cars = current_user.company.cars
-    @recent_service = current_user.company.car_maintenances
+    @pending_order = @company.pending_order
+    @cars = @company.cars
+    @recent_service = @company.car_maintenances
+      .includes(car: :car_model)
       .order(performed_at: :desc)
       .first
+  end
+
+  private
+
+  def set_company
+    @company = current_user.company
   end
 end
