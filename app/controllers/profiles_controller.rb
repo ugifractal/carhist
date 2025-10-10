a55f2373f77279f1f6a3f75d6480b7052c8635b9
tsaqib29 <tsaqibarib@.com>
2025-10-10 15:17:39 +0700

    [#454] typo languange to language

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -16,6 +16,6 @@ class ProfilesController < ApplicationController
   private
 
   def profile_params
-    params.expect(user: %i[ timezone image languange])
+    params.expect(user: %i[ timezone image language])
   end
 end
--- db/migrate/20251009141604_add_languange_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLanguangeToUsers < ActiveRecord::Migration[8.0]
-  def change
-    add_column :users, :languange, :string
-  end
-end
