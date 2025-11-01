b7a24932c21c58fbdd4f3cff7acb2fd45f3e6bca
tsaqib29 <tsaqibarib@.com>
2025-11-01 19:02:00 +0700

    [#454] change i18n

--- db/migrate/20251010081632_add_language_to_users.rb
+++ db/migrate/20251010081632_add_language_to_users.rb
@@ -1,5 +1,5 @@
 class AddLanguageToUsers < ActiveRecord::Migration[8.0]
   def change
-    add_column :users, :language, :string
+    add_column :users, :language, :string, default: "id"
   end
 end
