a87872dbd53950d098bcdc6b8f2aa1275970f07b
tsaqib29 <tsaqibarib@.com>
2025-11-04 17:23:48 +0700

    [#454] fix error test system

--- db/schema.rb
+++ db/schema.rb
@@ -44,10 +44,10 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_01_223610) do
 
   create_table "api_keys", force: :cascade do |t|
     t.string "api_key"
-    t.string "company_id"
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "car_brands", force: :cascade do |t|
