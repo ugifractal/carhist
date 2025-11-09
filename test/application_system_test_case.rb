bdf33aee464f22266b9ea77f0051807b604d6dd5
ugifractal <ugidmtest@gmail.com>
2025-11-09 13:54:27 +0700

    update test

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,11 +2,10 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: (ENV["CI"].present? ? :headless_chrome : :chrome), screen_size: [ 1400, 1400 ]
+  include AbstractController::Translation
 
   def login_as(user)
     super(user)
     I18n.locale = user.language.to_sym
   end
-
-  include AbstractController::Translation
 end
