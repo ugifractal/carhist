111a387c9e5834a2dbeb47ee3b8458cc5867c1b5
ugifractal <ugidmtest@gmail.com>
2026-01-27 19:19:15 +0700

    Allow edit company name

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,27 @@
 class CompaniesController < ApplicationController
-    def show
-        @company = current_user.company
+  def show
+    @company = current_user.company
+  end
+
+  def edit
+    @company = current_user.company
+  end
+
+  def update
+    @company = current_user.company
+    if @company.update(company_params)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to company_path, notice: 'Company was successfully updated.' }
+      end
+    else
+      render :edit, status: :unprocessable_entity
     end
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name)
+  end
 end
